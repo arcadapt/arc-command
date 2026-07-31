9258ca0c94e7a2ab6b9224b8e4e3f4698ff1e8f8
PotatoSpark <reece.francis6@gmail.com>
2026-07-31 10:01:22 +1000

    Next pass 30 Jul: bigger header logo, one size across all three apps (sw acmd-v3)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "acmd-v2";
+const CACHE = "acmd-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./favicon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
