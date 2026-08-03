96050f82cf6ab7a73cdc7abdeb1355ccf5e90862
PotatoSpark <reece.francis6@gmail.com>
2026-08-04 09:49:02 +1000

    V0.20 beta - lockstep with Pass 17 (acmd-v22)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "acmd-v21";
+const CACHE = "acmd-v22";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./favicon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
